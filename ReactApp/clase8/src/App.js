eeee51719b7fa3274e385711a58b1db4107130dd
MAGA1314 <garciaagudelomiguelangel@gmail.com>
2024-04-17 13:23:28 -0500

    malito

--- ReactApp/clase8/src/App.js
+++ ReactApp/clase8/src/App.js
@@ -3,10 +3,14 @@ import './App.css';
 import Contador from './Modulos/contador';
 
 function App() {
+  const usuarioUno = {
+    cuenta: 80,
+    edad: 12
+  }
   return (
     <div>
         <main>
-          <Contador />
+          <Contador cuenta = {usuarioUno.cuenta} edad = {usuarioUno.cuenta} />
         </main>
       
         <div>
--- ReactApp/clase8/src/Modulos/contador.js
+++ ReactApp/clase8/src/Modulos/contador.js
@@ -1,13 +1,16 @@
 import React ,{useState} from "react";
 // hook --> useState
-const Contador = () => {
-    const [cuenta, setCuenta] = useState(0);
+const Contador = (props) => {
+    const [cuenta, setCuenta] = useState(props.count);
+    const [edad, setEdad] = useState(props.age);
     return(
         <>
             <p>Presionaste el boton {cuenta} veces</p>
-            <button onClick={()=>setCuenta(cuenta++)}>
+            <button onClick={()=>setCuenta(cuenta+1)}>
                 Haz Click
             </button>
+            <p>Tu edad es {edad} años</p>
+            <button onClick={()=>setEdad}></button>
         </>
     )
 }
